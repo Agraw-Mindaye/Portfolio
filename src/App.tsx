8da95e121e3fe83f7d4442802b88b57a97868aa2
Agraw Mindaye <agraw@Aminats-MacBook-Pro.local>
2025-01-21 07:58:24 -0500

    made styling edits to Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import "./styles/App.css";
 
 export default function App() {
   return (
     <main>
       <Navbar />
-      <About />
-      <Projects />
-      <Contact />
+      <div className="content-container">
+        <About />
+        <Projects />
+        <Contact />
+      </div>
     </main>
   );
 }
